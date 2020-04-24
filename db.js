46bff3708452285d2ef1e01efdbff50b40e15e45
Abhi1006 <53849266+Abhi1006@users.noreply.github.com>
2020-04-24 22:44:46 +0200

    appiness interview code

---
+++ db.js
@@ -0,0 +1,12 @@
+//Database configuration
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/PaytmDB', (err)=>{
+    if(!err){
+        console.log('mongodb connection suceeded..');
+    }else{
+        console.log('Error in db connection..');
+    }
+});
+
+module.exports = mongoose;
